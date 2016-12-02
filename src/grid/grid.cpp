d46d9bc720a550b5dd209e220f7ec4b8974e06f3
Jeff Lestz <jlestz@pppl.gov>
2016-12-02 18:42:48 -0500

    minor cleanup of grid.cpp

--- src/grid/grid.cpp
+++ src/grid/grid.cpp
@@ -10,35 +10,3 @@ Grid::~Grid() {
     
     delete [] sliceTmp_; 
 } 
-
-	int evolveFields (double dt);
-	int getFieldInterpolatorVec (int cellID, * double InterpolatorVec);
-	int getCellID(double x, double y, double z);
-
- 	const int nx_;     // number of (physical + ghost) gridpoints  
- 	const int ny_;
- 	const int nz_;
-
-    const int nGhosts_; // number of ghost points in each direction
-
-    const double x0_;	// initial x position
- 	const double y0_;	// initial y position
- 	const double z0_;	// initial z position
-    
- 	const double dx_;
-
- 	double ***Ex_;
- 	double ***Ey_;
- 	double ***Ez_;
-
- 	double ***Bx_;
- 	double ***By_;
- 	double ***Bz_;
-
-  	double ***Bx_tm1_; //timestep back (since B at half timesteps)
- 	double ***By_tm1_;
- 	double ***Bz_tm1_;
-
-  	double ***Jx_;
- 	double ***Jy_;
- 	double ***Jz_;
