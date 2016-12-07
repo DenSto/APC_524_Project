a59d1254e1629df0ff640ad96705cf760fce180c
aglasser17 <alexander.glasser@gmail.com>
2016-12-07 07:44:00 -0500

    initial deposit files

---
+++ src/particles/deposit.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "deposit.hpp"
+
+Depositor::Depositor(){
+}
+
+Depositor::~Depositor(){
+}
+
+void Depositor::deposit_particle() {
+
+}
---
+++ src/particles/deposit.hpp
@@ -0,0 +1,11 @@
+#ifndef DEPOSIT_HPP
+#define DEPOSIT_HPP
+
+class Depositor{
+ public:
+  Depositor();
+  ~Depositor();
+  void deposit_particle(); //deposit particle on cell (Rho, J)
+};
+
+#endif
