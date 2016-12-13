660f9441516ca4675a1e02b789978b2a28cc5a28
Noah Mandell <nrmandell@gmail.com>
2016-12-13 13:40:43 -0500

    added a debug parameter to input file, and added if(debug) to fprintf statements in main. intention is for debug=1 to be basic runtime prints, debug>1 to be more verbose.

--- src/grid/grid.cpp
+++ src/grid/grid.cpp
@@ -42,7 +42,6 @@ Grid::Grid(int *nxyz, int nGhosts, double *xyz0, double *Lxyz):
     nFieldsTotal_(15), 
     ghostVecSize_(nFieldsToSend_*nRealPtsYZPlane_)
 {
-    fprintf(stderr,"New Grid Object\n");
     checkInput_(); 
 
     sliceTmp_ = new double[ghostVecSize_/nFieldsToSend_]; 
