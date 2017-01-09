80ec28d4815ec2f2caa5456a655e317ef28dfee3
Noah Mandell <nrmandell@gmail.com>
2017-01-09 12:09:41 -0500

    adding more to hdf5io unit test

--- test/test_src/hdf5io_unittests.cc
+++ test/test_src/hdf5io_unittests.cc
@@ -34,8 +34,8 @@ TEST_F(FieldIOTest, writeField) {
     double xyz0 [3] = {0,0,0};
     double Lxyz [3] = {1,1,1};
 
-    int which_fields = 0; //only write rho
-    int nwrite = 2;
+    int which_fields = 1; //only write E
+    const int nwrite = 2;
 
     size_MPI = 1;
     rank_MPI = 0;
@@ -48,14 +48,17 @@ TEST_F(FieldIOTest, writeField) {
     for (i=0; i<grid->nx_+1; ++i) { 
         for (j=0; j<grid->ny_+1; ++j) { 
             for (k=0; k<grid->nz_+1; ++k) { 
-                grid->rho_[i][j][k] = -( k + 10*j + 100*i ); 
+                grid->Ex_[i][j][k] = -( k + 10*j + 100*i ); 
+                grid->Ey_[i][j][k] = -10*( k + 10*j + 100*i ); 
+                grid->Ez_[i][j][k] = -100*( k + 10*j + 100*i ); 
             }
         }
     }
     field_tsio->writeFields(grid, which_fields, 0);
 
+    // change Ex
     int dim_phys[3];
-    grid->getDimPhys(grid->getrhoID(), dim_phys); 
+    grid->getDimPhys(grid->getExID(), dim_phys); 
 
     int iBeg = 1;
     int jBeg = 1;
@@ -67,25 +70,43 @@ TEST_F(FieldIOTest, writeField) {
     for (i=iBeg; i<iEnd; ++i) { 
         for (j=jBeg; j<jEnd; ++j) { 
             for (k=kBeg; k<kEnd; ++k) { 
-                grid->rho_[i][j][k] = (k) + 10*(j) + 100*(i); 
+                grid->Ex_[i][j][k] = (k) + 10*(j) + 100*(i); 
             }
         }
     }
     field_tsio->writeFields(grid, which_fields, 1);
 
-    // check
+    // check file
     hid_t file_id = hdf5io->getFileID();
-    hid_t dset_id = H5Dopen2(file_id, "/fields/rho", H5P_DEFAULT);
+    ASSERT_GE(file_id, 0);
+    hid_t dset_id;
+    dset_id = H5Dopen2(file_id, "/fields/Ey", H5P_DEFAULT);
+    ASSERT_GE(dset_id, 0);
+    dset_id = H5Dopen2(file_id, "/fields/Ez", H5P_DEFAULT);
+    ASSERT_GE(dset_id, 0);
+    // check Ex values
+    dset_id = H5Dopen2(file_id, "/fields/Ex", H5P_DEFAULT);
+    ASSERT_GE(dset_id, 0);
     hid_t filespace = H5Dget_space(dset_id);
+    ASSERT_GE(filespace, 0);
 
-    double* check_data = new double[dim_phys[0]*dim_phys[1]*dim_phys[2]*2];
+    hsize_t* dims = new hsize_t[4];
+    hsize_t* maxdims = new hsize_t[4];
+    H5Sget_simple_extent_dims(filespace, dims, maxdims);
+
+    ASSERT_EQ(dims[0], dim_phys[0]);
+    ASSERT_EQ(dims[1], dim_phys[1]);
+    ASSERT_EQ(dims[2], dim_phys[2]);
+    ASSERT_EQ(dims[3], nwrite);
+
+    double* check_data = new double[dim_phys[0]*dim_phys[1]*dim_phys[2]*nwrite];
     H5Dread(dset_id, H5T_NATIVE_DOUBLE, H5S_ALL, filespace, H5P_DEFAULT, check_data);
 
     for (i=iBeg; i<iEnd; ++i) { 
         for (j=jBeg; j<jEnd; ++j) { 
             for (k=kBeg; k<kEnd; ++k) { 
-                   EXPECT_EQ(-grid->rho_[i][j][k], check_data[0 + nwrite*(k-1) + nwrite*dim_phys[2]*(j-1) + nwrite*dim_phys[2]*dim_phys[1]*(i-1)]);
-                   EXPECT_EQ(grid->rho_[i][j][k], check_data[1 + nwrite*(k-1) + nwrite*dim_phys[2]*(j-1) + nwrite*dim_phys[2]*dim_phys[1]*(i-1)]);
+                   EXPECT_EQ(-grid->Ex_[i][j][k], check_data[0 + nwrite*(k-1) + nwrite*dim_phys[2]*(j-1) + nwrite*dim_phys[2]*dim_phys[1]*(i-1)]);
+                   EXPECT_EQ(grid->Ex_[i][j][k], check_data[1 + nwrite*(k-1) + nwrite*dim_phys[2]*(j-1) + nwrite*dim_phys[2]*dim_phys[1]*(i-1)]);
             }
         }
     }
