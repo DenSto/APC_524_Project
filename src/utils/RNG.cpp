a88cf09227e9d98047a7354b1a88cbaf19ab872f
DenSto <dstonge@pppl.gov>
2021-12-30 22:57:40 +0000

    Changed indents to spaces in RNG

--- src/utils/RNG.cpp
+++ src/utils/RNG.cpp
@@ -21,16 +21,16 @@
 #define LINE_LENGTH 2000
 
 Random_Number_Generator::Random_Number_Generator(long int seed){
-	state_ = (RNG_State*) malloc(sizeof(RNG_State));
-	state_->initialSeed = seed;
-	state_->idum = seed;
-  	long int* idum=&(state_->idum);
-	state_->idum2=123456789;
+  state_ = (RNG_State*) malloc(sizeof(RNG_State));
+  state_->initialSeed = seed;
+  state_->idum = seed;
+    long int* idum=&(state_->idum);
+  state_->idum2=123456789;
     state_->iy=0;
     if ( *idum == 0)
-		*idum=1; /* Be sure to prevent idum = 0 */
+    *idum=1; /* Be sure to prevent idum = 0 */
     else if(*idum < 0) 
-		*idum = -(*idum);
+    *idum = -(*idum);
     state_->idum2=(*idum);
     for (int j=RNG_NTAB+7;j>=0;j--) { /* Load the shuffle table (after 8 warm-ups) */
       long int k=(*idum)/IQ1;
@@ -39,25 +39,25 @@ Random_Number_Generator::Random_Number_Generator(long int seed){
       if (j < RNG_NTAB) state_->iv[j] = *idum;
     }
     state_->iy=state_->iv[0];
-	state_->generate=true;
+  state_->generate=true;
 
-	userCDF_ = NULL;
-	userPDF_ = NULL;
-	userVal_ = NULL;
+  userCDF_ = NULL;
+  userPDF_ = NULL;
+  userVal_ = NULL;
 }
 
 Random_Number_Generator::~Random_Number_Generator(){
-	free(state_);
-	freeUserData();
+  free(state_);
+  freeUserData();
 }
 
 void Random_Number_Generator::freeUserData(){
-	if(userVal_ != NULL)
-		free(userVal_);
-	if(userCDF_ != NULL)
-		free(userCDF_);
-	if(userPDF_ != NULL)
-		free(userPDF_);
+  if(userVal_ != NULL)
+    free(userVal_);
+  if(userCDF_ != NULL)
+    free(userCDF_);
+  if(userPDF_ != NULL)
+    free(userPDF_);
 }
 
 //! Get a random number in the range (0,1), exclusive
@@ -70,7 +70,7 @@ double Random_Number_Generator::getUniform(){
   long int k;
   long int* idum;
   double temp;
-	
+  
   idum=&(state_->idum);
   
   k=(*idum)/IQ1;                 /* Start here when not initializing */
@@ -102,12 +102,12 @@ double Random_Number_Generator::getUniform(){
 
 //! Draw a random number, inclusive of both min and max
 long Random_Number_Generator::getInteger(long min, long max){
-	return min + (long)floor((max + 1.0 - min)*getUniform());
+  return min + (long)floor((max + 1.0 - min)*getUniform());
 }
 
 //! Draw a number from a standard normal distribution.
 double Random_Number_Generator::getStandardNormal(){
-	return getGaussian(0.0,1.0);
+  return getGaussian(0.0,1.0);
 }
 
 //! Draw a number from a normal distribution.
@@ -116,63 +116,63 @@ double Random_Number_Generator::getStandardNormal(){
  * so store both, return one at a time.
  */
 double Random_Number_Generator::getGaussian(double mu, double sigma){
-	const double epsilon = std::numeric_limits<double>::min();
-	const double two_pi = 2.0*3.14159265358979323846;
+  const double epsilon = std::numeric_limits<double>::min();
+  const double two_pi = 2.0*3.14159265358979323846;
 
-	if (!state_->generate){ // just return the stored second number
-		state_->generate = !state_->generate;
-		return state_->z1 * sigma + mu;
-	}
+  if (!state_->generate){ // just return the stored second number
+    state_->generate = !state_->generate;
+    return state_->z1 * sigma + mu;
+  }
 
-	double u1, u2;
-	// Do loop to avoid u1 equaling to zero. 
-	// Should never happen as ran2 returns between 0 and 1 exclusively
-	do {
-		u1 = getUniform();
-		u2 = getUniform();
-	} while ( u1 <= epsilon ); 
+  double u1, u2;
+  // Do loop to avoid u1 equaling to zero. 
+  // Should never happen as ran2 returns between 0 and 1 exclusively
+  do {
+    u1 = getUniform();
+    u2 = getUniform();
+  } while ( u1 <= epsilon ); 
 
-	state_->z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
-	state_->z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
+  state_->z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
+  state_->z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
 
-	state_->generate = !state_->generate;
+  state_->generate = !state_->generate;
 
-	return state_->z0 * sigma + mu;
+  return state_->z0 * sigma + mu;
 }
 
 void Random_Number_Generator::setRNGState(RNG_State* state){
-	state_=state;
+  state_=state;
 }
 
 RNG_State* Random_Number_Generator::getRNGState(){
-	return state_;
+  return state_;
 }
 
 
 void Random_Number_Generator::setUserPDF(bool isDiscrete, long size, double* userVal, double* userProb){
-	freeUserData();
-	userVal_ = (double*) calloc(size,sizeof(double));	
-	userCDF_ = (double*) calloc(size,sizeof(double));	
-	userSize_ = size;
-	userIsDiscrete_ = isDiscrete;
+  freeUserData();
+  userVal_ = (double*) calloc(size,sizeof(double));  
+  userCDF_ = (double*) calloc(size,sizeof(double));  
+  userSize_ = size;
+  userIsDiscrete_ = isDiscrete;
 
-	if(!isDiscrete) 
-		userPDF_ = (double*) calloc(size,sizeof(double));	
+  if(!isDiscrete) 
+    userPDF_ = (double*) calloc(size,sizeof(double));  
 
-	for(int i = 0; i < size; i++){
-		assert(userProb[i] >= 0);
-		userVal_[i] = userVal[i];
-		userCDF_[i] = userProb[i];
-		if(!isDiscrete) 
-			userPDF_[i] = userProb[i];
-	}
-	computeUserCDF();
+  for(int i = 0; i < size; i++){
+    assert(userProb[i] >= 0);
+    userVal_[i] = userVal[i];
+    userCDF_[i] = userProb[i];
+    if(!isDiscrete) 
+      userPDF_[i] = userProb[i];
+  }
+  computeUserCDF();
 }
 
 //! Load a user distribution from file.
 /*!
- *	File is in ascii format with two columns. First column representes the value while 
- *	the second column represents the probability associated with that value. 
+ *  File is in ascii format with two columns. First column representes the value while 
+ *  the second column represents the probability associated with that value. 
  *
  *  Values do not need to be equally spaced.
  *
@@ -180,126 +180,126 @@ void Random_Number_Generator::setUserPDF(bool isDiscrete, long size, double* use
  *  are continuous in both the zeroth and first derivatives.
  */
 void Random_Number_Generator::loadUserPDFfromFile(const bool isDiscrete,const char* fname){
-	freeUserData();
-	FILE* in = fopen(fname,"r");
-	char buf[LINE_LENGTH];
-	assert(in != NULL);
+  freeUserData();
+  FILE* in = fopen(fname,"r");
+  char buf[LINE_LENGTH];
+  assert(in != NULL);
 
-	userSize_=0;
-	userIsDiscrete_ = isDiscrete;
+  userSize_=0;
+  userIsDiscrete_ = isDiscrete;
 
-	//get line count excluding comments
-	//ensure we allocate the correct amount of memory
-	fgets(buf,LINE_LENGTH,in);
-	while(buf[0] != '\0' && !feof(in)){
-		if(buf[0] != '#'){
-			userSize_++;
-		}
-		fgets(buf,LINE_LENGTH,in);
-	}
+  //get line count excluding comments
+  //ensure we allocate the correct amount of memory
+  fgets(buf,LINE_LENGTH,in);
+  while(buf[0] != '\0' && !feof(in)){
+    if(buf[0] != '#'){
+      userSize_++;
+    }
+    fgets(buf,LINE_LENGTH,in);
+  }
 
-	// allocate the memory
-	userVal_ = (double*) calloc(userSize_,sizeof(double));	
-	userCDF_ = (double*) calloc(userSize_,sizeof(double));	
-	if(!isDiscrete)
-		userPDF_ = (double*) calloc(userSize_,sizeof(double));	
+  // allocate the memory
+  userVal_ = (double*) calloc(userSize_,sizeof(double));  
+  userCDF_ = (double*) calloc(userSize_,sizeof(double));  
+  if(!isDiscrete)
+    userPDF_ = (double*) calloc(userSize_,sizeof(double));  
 
-	rewind(in);
+  rewind(in);
 
-	// read in actual data
-	fgets(buf,LINE_LENGTH,in);
-	int i =0;
-	while(buf[0] != '\0' && !feof(in)){
-		if(buf[0] != '#'){
-			sscanf(buf,"%lf %lf\n",&userVal_[i],&userCDF_[i]);
-			assert(userCDF_[i] >= 0);
-			if(!isDiscrete) 
-				userPDF_[i] = userCDF_[i];
-			i++;
-		}
-		fgets(buf,LINE_LENGTH,in);
-	}
+  // read in actual data
+  fgets(buf,LINE_LENGTH,in);
+  int i =0;
+  while(buf[0] != '\0' && !feof(in)){
+    if(buf[0] != '#'){
+      sscanf(buf,"%lf %lf\n",&userVal_[i],&userCDF_[i]);
+      assert(userCDF_[i] >= 0);
+      if(!isDiscrete) 
+        userPDF_[i] = userCDF_[i];
+      i++;
+    }
+    fgets(buf,LINE_LENGTH,in);
+  }
 
-	// calculate CDF from PDF
-	computeUserCDF();
+  // calculate CDF from PDF
+  computeUserCDF();
 
-	fclose(in);
+  fclose(in);
 }
 
 void Random_Number_Generator::computeUserCDF(){
-	// Given PDF, calculate CDF
-	if(userIsDiscrete_){ // Discrete, use bins
-		for(int i = 1; i < userSize_; i++){
-			userCDF_[i] += userCDF_[i-1];
-		}
-	} else { // Continuous, use trapezoidal rule
+  // Given PDF, calculate CDF
+  if(userIsDiscrete_){ // Discrete, use bins
+    for(int i = 1; i < userSize_; i++){
+      userCDF_[i] += userCDF_[i-1];
+    }
+  } else { // Continuous, use trapezoidal rule
 
-		userCDF_[0] = 0.0;
-		for(int i = 1; i < userSize_; i++){
-			userCDF_[i] = 0.5*(userPDF_[i] + userPDF_[i-1]) + userCDF_[i-1];
-		}
-	}
+    userCDF_[0] = 0.0;
+    for(int i = 1; i < userSize_; i++){
+      userCDF_[i] = 0.5*(userPDF_[i] + userPDF_[i-1]) + userCDF_[i-1];
+    }
+  }
 
-	// Ensure normalization (0 to 1)
-	for(int i = 0; i < userSize_; i++){
-		if(!userIsDiscrete_)
-			userPDF_[i] /= userCDF_[userSize_ - 1];	
-		userCDF_[i] /= userCDF_[userSize_ - 1];	
-	}
+  // Ensure normalization (0 to 1)
+  for(int i = 0; i < userSize_; i++){
+    if(!userIsDiscrete_)
+      userPDF_[i] /= userCDF_[userSize_ - 1];  
+    userCDF_[i] /= userCDF_[userSize_ - 1];  
+  }
 }
 
 //! Get a random number from the user distribution.
 /*!
- *	Uses binary search ( O(log n) ) and quadratic interpolation for 
- *	continuous distributions.
+ *  Uses binary search ( O(log n) ) and quadratic interpolation for 
+ *  continuous distributions.
  *
  */
 double  Random_Number_Generator::getUserNumber(){
-	assert(userVal_ != NULL && userCDF_ != NULL);
+  assert(userVal_ != NULL && userCDF_ != NULL);
 
-	int L = 0, R = userSize_ - 1;
-	int i = (R + L)/2;
-	double uni = getUniform();
+  int L = 0, R = userSize_ - 1;
+  int i = (R + L)/2;
+  double uni = getUniform();
 
 
-	if(userIsDiscrete_){//the the bin number
-		while(L != R){
-			if(uni < userCDF_[i])
-				R = i;
-			else
-				L = i + 1;
-			i = (R + L)/2;
-		}
-		return userVal_[i];
-	} else {
-		//Binary Search
-		while(!(userCDF_[i] <= uni && userCDF_[i + 1] > uni)){
-			if(uni < userCDF_[i])
-				R = i;
-			else
-				L = i;
-			i = (R + L)/2;
-		}
+  if(userIsDiscrete_){//the the bin number
+    while(L != R){
+      if(uni < userCDF_[i])
+        R = i;
+      else
+        L = i + 1;
+      i = (R + L)/2;
+    }
+    return userVal_[i];
+  } else {
+    //Binary Search
+    while(!(userCDF_[i] <= uni && userCDF_[i + 1] > uni)){
+      if(uni < userCDF_[i])
+        R = i;
+      else
+        L = i;
+      i = (R + L)/2;
+    }
 
-		// If continuous, quadractically interpolate 
-		// (partial integration of triangle rule) 
+    // If continuous, quadractically interpolate 
+    // (partial integration of triangle rule) 
 
-		double c1,c2,a,b,cdf,dis,sqroot;
+    double c1,c2,a,b,cdf,dis,sqroot;
 
-		cdf= userCDF_[i];
-		c1 = userVal_[i];
-		c2 = userVal_[i+1];
-		a = userPDF_[i];
-		b = userPDF_[i+1];
+    cdf= userCDF_[i];
+    c1 = userVal_[i];
+    c2 = userVal_[i+1];
+    a = userPDF_[i];
+    b = userPDF_[i+1];
 
-		// discriminant of quadratic equation 
-		dis = (c1-c2)*((a*a)*(c1-c2) + 2*(uni - cdf)*(a-b)); 
-		assert(dis > 0); // this should always be true
-		sqroot = sqrt(dis);	
+    // discriminant of quadratic equation 
+    dis = (c1-c2)*((a*a)*(c1-c2) + 2*(uni - cdf)*(a-b)); 
+    assert(dis > 0); // this should always be true
+    sqroot = sqrt(dis);  
 
-		// we need the positive root here
-		return (b*c1 - a*c2 + sqroot)/(b-a);
-	}
+    // we need the positive root here
+    return (b*c1 - a*c2 + sqroot)/(b-a);
+  }
 }
 
 
--- src/utils/RNG.hpp
+++ src/utils/RNG.hpp
@@ -8,20 +8,20 @@
  * Can be written/read using fwrite/fread.
  */
 typedef struct RNG_State{
-	long int initialSeed;
-	long int idum;
-	long int idum2;
-	long int iy;
-	long int iv[RNG_NTAB];
-	double z0,z1;
-	bool generate;
+  long int initialSeed;
+  long int idum;
+  long int idum2;
+  long int iy;
+  long int iv[RNG_NTAB];
+  double z0,z1;
+  bool generate;
 }RNG_State;
 
 
 //! Class that provides methods to generate random numbers
 /*!
  * 
- *	The Random Number generator class uses the ran2 algorithm from Numerical recipes.
+ *  The Random Number generator class uses the ran2 algorithm from Numerical recipes.
  *  The algorithm provides fast random numbers over (0,1) exclusive with a period of over 10^15.
  * 
  *  This is then used in the implementation for numerous other distrituions (standard normal, integer...)
@@ -36,32 +36,32 @@ typedef struct RNG_State{
  * 
  */
 class Random_Number_Generator{
-	public:
-		Random_Number_Generator(long seed); 
-		~Random_Number_Generator();
-		double getUniform();// Get a number between 0 and 1 exclusive
-		long getInteger(long min, long max);// Get an integer between min and max
-		double getStandardNormal(); // Get a number from a standard normal (zero mean, unit variance)
-		double getGaussian(double mu, double sigma); // Get a number from an arbitrary gaussian
-		RNG_State *getRNGState();  // get the current RNG state
-		void setRNGState(RNG_State* state); // set the current RNG state
+  public:
+    Random_Number_Generator(long seed); 
+    ~Random_Number_Generator();
+    double getUniform();// Get a number between 0 and 1 exclusive
+    long getInteger(long min, long max);// Get an integer between min and max
+    double getStandardNormal(); // Get a number from a standard normal (zero mean, unit variance)
+    double getGaussian(double mu, double sigma); // Get a number from an arbitrary gaussian
+    RNG_State *getRNGState();  // get the current RNG state
+    void setRNGState(RNG_State* state); // set the current RNG state
 
-		void setUserPDF(bool isDiscrete,long size, double* userVal, double* userProb); // set a user-defined distribution
-																					   // both userVal and userProb must be 
-																					   // of size 'size'.
-	
-		void loadUserPDFfromFile(const bool isDiscrete, const char* fname); // load a user-defined distribution from a file
-		double getUserNumber();// draw a number from user-defined distribution
+    void setUserPDF(bool isDiscrete,long size, double* userVal, double* userProb); // set a user-defined distribution
+                                             // both userVal and userProb must be 
+                                             // of size 'size'.
+  
+    void loadUserPDFfromFile(const bool isDiscrete, const char* fname); // load a user-defined distribution from a file
+    double getUserNumber();// draw a number from user-defined distribution
 
 
-	private:
-		RNG_State *state_;
-		double* userVal_;
-		double* userCDF_;
-		double* userPDF_; // needed for continous PDF for quadratic interpolation
-		long userSize_;
-		bool userIsDiscrete_;
-		void computeUserCDF();
-		void freeUserData();
+  private:
+    RNG_State *state_;
+    double* userVal_;
+    double* userCDF_;
+    double* userPDF_; // needed for continous PDF for quadratic interpolation
+    long userSize_;
+    bool userIsDiscrete_;
+    void computeUserCDF();
+    void freeUserData();
 };
 #endif
